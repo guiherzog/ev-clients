c057043d43973eb2c9e4074d62e72a34d2fb97b7
Guilherme Herzog <guilhermeherzog@gmail.com>
2018-03-22 12:39:00 -0300

    Removing clients from DB

--- src/views/TableList/TableList.jsx
+++ src/views/TableList/TableList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from 'react';
 import { Grid } from "material-ui";
 import { connect } from 'react-redux';
 
@@ -6,36 +6,51 @@ import { RegularCard, ClientTable, ItemGrid } from "components";
 
 import { getClients, editClient, removeClient } from '../../actions/actions';
 
-function TableList({ ...props }) {
-  return (
-    <Grid container>
-      <ItemGrid xs={12} sm={12} md={12}>
-        <RegularCard
-          headerColor="blue"
-          cardTitle="All Active Clients"
-          cardSubtitle="Here you can add, edit or remove any clients"
-          content={
-            <ClientTable
-              tableHeaderColor="info"
-              tableHead={["ID", "Name", "Email", "Phone", "Address"]}
-              tableData={[ ...props.clients ]}
-              removeItem={(id) => console.log('Removing client '+id)}
-              editItem={(id) => console.log('Editing client '+id)}
-            />
-          }
-        />
-      </ItemGrid>
-    </Grid>
-  );
+class TableList extends Component{
+  componentDidMount(){
+    this.props.getClients();
+  }
+
+  render(){
+    const props = this.props;
+    console.log(this.props);
+    return (
+      <Grid container>
+        <ItemGrid xs={12} sm={12} md={12}>
+          <RegularCard
+            headerColor="blue"
+            cardTitle="All Active Clients"
+            cardSubtitle="Here you can add, edit or remove any clients"
+            content={
+              <ClientTable
+                tableHeaderColor="info"
+                tableHead={["ID", "Name", "Email", "Phone", "Address"]}
+                tableData={this.props.clients}
+                removeItem={(id) => props.removeClient(id)}
+                editItem={(id) => props.editClient(id)}
+              />
+            }
+          />
+        </ItemGrid>
+      </Grid>
+    );
+  }
 }
 
-const mapStateToProps = state => ({
-  clients: getClients().clients
-})
+TableList.defaultProps = {
+  clients: [],
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return ({
+    clients: state.clients
+  })
+}
 
 const mapDispatchToProps = dispatch => ({
   editClient: id => dispatch(editClient(id)),
-  removeClient: id => dispatch(removeClient(id))
+  removeClient: id => dispatch(removeClient(id)),
+  getClients: () => dispatch(getClients())
 })
 
 export default connect(
